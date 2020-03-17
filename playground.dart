d9a1663ac7f7732fbb8da21cbb3b64868bc7becc
Ashmin Antony <51887428+AshminAntony@users.noreply.github.com>
2020-03-18 01:45:39 +0530

    Update playground.dart

--- playground.dart
+++ playground.dart
@@ -1,8 +1,12 @@
+import 'dart:html';
 import 'dart:io';
 
 class Tech{
   Set <String> interest;
   bool mentor;
+  var name;
+  var id;
+  static List mentorname;
 
   
   addStacks(var value){
@@ -14,20 +18,29 @@ class Tech{
     stdout.write("Are you a Mentor(y/n): ");
     op = stdin.readLineSync();
     op=='y' ? mentor=true :(op=='n' ? mentor=false:print("Invalid Entry! enter y/n"));
+    if(mentor==true) mentorname.add(name);
   }
   setAvailableTime(){
 
   }
-  getMentor(){
+  getMentor(var interest,bool mentor){
+    int f=-1;
+    if(mentor!= true){
+      stdout.write("Searching mentor...");
+      for(int i=0; i<this.interest.length; ++i){
+      if(interest[i]==interest) f=i;
+      }
+      if(f>=0&&)
+    }
 
   }
 }
 
 main(){
-  Tech S1 = Tech();
+  Tech s = Tech();
   String ch;
   do{
-    stdout.write("Choose")
+    stdout.write("Choose: \n1. Add Stacks\n2. Set Mentor\n3.Availabe Time\n4. ");
 
   }while(i)
   
